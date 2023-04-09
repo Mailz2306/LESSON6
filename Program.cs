c823023f44914cb8eeb1cf6dd81808619a59f6ab
mailz <sergey4istyak@yandex.ru>
2023-04-09 14:38:40 +0400

    начал делать дом работу

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.InputFiles;
+
+TelegramBotClient client = new TelegramBotClient("fgfjhgfdjhgf_bot"); 
+
+User user = await client.GetMeAsync();
+
+Console.WriteLine(user.Username); 
+
